acf6f953ff179fd166eb93b2d20076fbff2f758d
MK-coder <52727959+MK-coder@users.noreply.github.com>
2019-08-06 17:25:34 -0400

    Add files via upload

---
+++ assets/data/myLoginName.ts
@@ -0,0 +1,9 @@
+import { YourLoginName } from '../../app/yourLoginName';
+
+export const MYLOGINNAME: YourLoginName = {
+    studentID: 991494220,
+    name: "Mark Caldeira",
+    login: "caldeima",
+    campus: "Davis",
+    title: "Assignment 4"
+};
